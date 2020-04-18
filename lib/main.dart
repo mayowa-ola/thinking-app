64264fb95c529ddcbb391bcea155080638ba605a
mayowa_ola <mayowaoladunjoye@gmail.com>
2020-04-18 14:17:00 +0100

    completed the magic ball challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[200],
+        ),
+        body: MagicBall(),
+      ),
+    ));
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int imageNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              imageNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$imageNumber.png'),
+        ),
       ),
     );
+  }
+}
